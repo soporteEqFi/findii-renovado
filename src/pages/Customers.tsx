a9409d179a9692bae8730c2388027318010e6a03
Jean <00zacks00@gmail.com>
2025-03-16 00:36:37 -0500

    update: se cambia posiciÂón del boton de descargar ventas

--- src/pages/Customers.tsx
+++ src/pages/Customers.tsx
@@ -184,16 +184,6 @@ const Customers = () => {
         <div className="p-6 border-b flex justify-between items-center">
           <h1 className="text-2xl font-semibold text-gray-800">Customers</h1>
           <div className="flex items-center space-x-4">
-            {/* Botón de descargar ventas*/}
-            {canDownloadSales() && (
-              <button
-                onClick={handleDownloadSales}
-              className="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-            >
-              <Download className="w-4 h-4 mr-2" />
-              Descargar Ventas
-            </button>
-            )}
             
             {user && (
               <span className="text-sm text-gray-600">
@@ -209,11 +199,21 @@ const Customers = () => {
         </div>
 
         {/* Botón Nuevo Cliente */}
-        <div className="flex justify-end p-4 bg-gray-50 border-b">
+        <div className="flex justify-end p-4 bg-gray-50 border-b gap-4">
+            {/* Botón de descargar ventas*/}
+            {canDownloadSales() && (
+              <button
+                onClick={handleDownloadSales}
+              className="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-white bg-green-500 hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+            >
+              <Download className="w-4 h-4 mr-2" />
+              Descargar Ventas
+            </button>
+            )}
           {canCreateCustomer() && (
             <button
               onClick={() => setIsNewCustomerModalOpen(true)}
-              className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-600 hover:bg-green-700"
+              className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-500 hover:bg-green-600"
             >
               <Plus className="w-4 h-4 mr-2" />
               Nuevo Cliente
